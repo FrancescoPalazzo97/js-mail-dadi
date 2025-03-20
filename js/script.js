ec1ea5995ce35cc8a5eea4eb7e4fc4a489ac8089
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-03-20 15:44:17 +0100

    [UPD] Dichiarazioni variabili secondo esercizio + generazione casuale del numero dell'utente

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-const listaInvitati = [`mario.rossi@example.com`, `anna.bianchi@domain.com`, `luca.verdi@mail.net`, `francesca.russo@email.org`, `giuseppe.troisi@demo.com`, `sara.martini@sample.net`, `marco.delfino@test.org`, `elisa.ferrari@mailservice.com`, `alessandro.fontana@provider.net`, `laura.novelli@fakeemail.org`]; // Diachiaro una variabili che contiene la lista degli invitati
+// Esercizio delle mail
+alert(`Primo esercizio sulla lista delle mail`); // Avviso inizio primo esercizio
+console.log(`Primo esercizio sulla lista delle mail`); // Avviso inizio primo esercizio
+const listaInvitati = [
+    `mario.rossi@example.com`, 
+    `anna.bianchi@domain.com`, 
+    `luca.verdi@mail.net`, 
+    `francesca.russo@email.org`, 
+    `giuseppe.troisi@demo.com`, 
+    `sara.martini@sample.net`, 
+    `marco.delfino@test.org`, 
+    `elisa.ferrari@mailservice.com`, 
+    `alessandro.fontana@provider.net`, 
+    `laura.novelli@fakeemail.org`]; // Diachiaro una variabili che contiene la lista degli invitati
 const myMail = prompt(`Inserisci la tua mail`); // Dichiaro una variabile che contiene la mail dell'utente che inserisce tramite prompt
 let flag = false; // Diachiaro una variabile che mi serve come check
 let index; // Diachiaro una varibile da usare sucessivamente per trovare l'indice
@@ -14,6 +27,19 @@ for (let i = 0; i < listaInvitati.length && !flag; i++){ // Inizio un ciclo che 
 
 if (flag) { // Apro blocco if che mi stampa se ha trovato l'elemento oppure no
     console.log(`Elemento trovato in posizione ${index}`); // Se flag è true l'elemento è stato trovato
+    alert(`Elemento trovato in posizione ${index}`); // Stampa tramite alert
 } else { // Altrimenti
     console.log(`Elemento non trovato`); // L'elemento non è stato trovato
+    alert(`Elemento non trovato`); // Stampa tramite alert
 } // Chiudo blocco if
+//FINE PRIMO ESERCIZIO
+
+//Esercizio gioco dei dadi
+alert(`Secondo esercizio sul gioco dei dadi`); // Avviso inizio secondo esercizio
+console.log(`Secondo esercizio sul gioco dei dadi`); // Avviso inizio secondo esercizio
+
+alert(`Benvenuto al gioco dei dadi!\nprocedi per tirare il dado\nIl numero sarà compreso tra 1 e 6`); // Benvenuto all'utente
+console.log(`Benvenuto al gioco dei dadi!\nprocedi per tirare il dado\nIl numero sarà compreso tra 1 e 6`); // Benvenuto all'utente
+let myNum = Math.floor(Math.random() * 6); // Genero un numero ccasuale per l'utente
+alert (`Il tuo numero è ${myNum}`); // Avviso l'utente su quale numero è uscito
+console.log(`Il tuo numero è ${myNum}`); // Avviso l'utente su quale numero è uscito

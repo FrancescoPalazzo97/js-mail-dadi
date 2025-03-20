04a8d28013cc5cb97f90359aaace1c878e97b551
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-03-20 14:30:59 +0100

    [UPD] Dichiarazione variabili

---
+++ js/script.js
@@ -0,0 +1,2 @@
+const listaInvitati = [`mario.rossi@example.com`, `anna.bianchi@domain.com`, `luca.verdi@mail.net`, `francesca.russo@email.org`, `giuseppe.troisi@demo.com`, `sara.martini@sample.net`, `marco.delfino@test.org`, `elisa.ferrari@mailservice.com`, `alessandro.fontana@provider.net`, `laura.novelli@fakeemail.org`]; // Diachiaro una variabili che contiene la lista degli invitati
+const myMail = prompt(`Inserisci la tua mail`) // Dichiaro una variabile che contiene la mail dell'utente che inserisce tramite prompt
